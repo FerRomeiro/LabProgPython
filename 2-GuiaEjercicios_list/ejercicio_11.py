2d7bc5c0a14558a44d555b23128d52f84fc7c946
FerRomeiro <02FernandoRomero@gmail.com>
2023-04-10 02:43:52 -0300

    repaso

--- 2-GuiaEjercicios_list/ejercicio_11.py
+++ 2-GuiaEjercicios_list/ejercicio_11.py
@@ -10,8 +10,16 @@ palabra = input("ingrese palabra: ")
 for palabras in lista_palabras:
 
     if palabras == palabra:
-        print("La palabra esta en la lista")
-        break
+        
+        for indice in range(len(lista_palabras)):
+            if lista_palabras[indice] == palabra:
+                bandera=True
+                #print(len(lista_palabras[2])) OSEA USO LEN(([0])) APARECE EL NUMERO DE LETRAS
+                #len() PARA SABER LA CANTIDAD DE ELEMENTOS
+                #range() LO USAMOS EN EL FOR COMO CONTADOR
+                break
 
-if palabra!=palabras:
+if bandera==True:
+    print("La palabra {} esta en la lista y esta en el indice {}".format(palabra,indice+1))
+else:
     print("La palabra no esta en la lista")
