c6a3ea59d9eabba9493bb640e02bc0d846535e50
FerRomeiro <02FernandoRomero@gmail.com>
2023-05-29 08:01:09 -0300

    repaso

--- Ejercitacion/Clase4_parcial/py_game.py
+++ Ejercitacion/Clase4_parcial/py_game.py
@@ -262,7 +262,8 @@ while flag_correr:
 					posicion_circulo[0]=posicion_circulo[1]+10
 	
 	pantalla.fill(color_celeste)
-	pygame.draw.circle(pantalla,(color_amarillo),posicion_circulo,80)
+	pygame.draw.circle(pantalla,(color_amarillo),posicion_circulo,80) #pantalla ES EL NOMBRE DE LA VENTANA, DONDE SE VA A DIBUJAR EL CIRCULO
+																#A TRAVES DE posicion_circulo JUEGO CON LOS TAMAÑOS EN EL IF Y 80 ES EL TAMAÑO DEL CIRCULO
 	#mostrar
 	pygame.display.flip()
 pygame.quit()
@@ -270,24 +271,190 @@ pygame.quit()
 
 
 
+#segunda parte de la clase
+
+import pygame 
+pygame.init() #LO INICIALIZAMOS 
+
+window=pygame.display.set_mode([500,500]) #screen es el objeto,y creamos una ventana  set_mode es el metodo
+running = True
+while running: 
+	for event in pygame.event.get(): 
+		if event.type == pygame.QUIT:
+			running = False
+
+	window.fill((255,255,255)) #defino con la tupla el rgb osea el color de la ventana, en este caso es color blanco
+
+	pygame.draw.circle(window,(0,0,255),(250,250),75)#WINDOW ES EL NOMBRE DE LA VENTANA DONDE SE VA A DIBUJAR EL CIRCULO,PRIMER(ES EL COLOR) Y SEG (POSICION)
+														#Y 75 ES EL TAMAÑO DEL CIRCULO OSEA LA RADIO
+
+	pygame.display.flip() #todos los cambios q hagamos se reconstruye en pantalla gracias a flip
+						#constantemente lo pinta de blanco y lo muestra y asi sucesivamente porque esta dentro del for hasta q sea False
 
 
+window = pygame.display.set_mode((500, 400), 0, 32)
+pygame.display.set_caption("vamos a hacer un juego!")
+while(running):
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
 
 
+#IMAGENES:png y jpg la diferencia es q la imagen png no tiene fondo, cortonea y la jpg es una imagen completa con fondo y todo
+#con el metodo load de la clase image se puede generar una superficie desde una imagen EN PNG TMB TIENE BORDES PERO SON INVISIBLES ES DECIR ES CUADRADO TMB
+#hay q crear una carpeta con las imagenes para usar el nombre y la extension q tiene y para usarla hay q hacer lo del ejemplo:
+#imagen=pygame.image.load("00.png")
+#print(type(image)) #class "pygame.Surface"
+#esto es lo q primero se debe hacer con las imagenes
+#Para copiar la superficie de la imagen en la superficie de la pantalla o en otra superficie se debe utilizar blit() indicando la ubicacion
+#imagen=pygame.image.load("00.png")  
+#print(type(image)) #class "pygame.Surface"
+#sceen.blit(imagen,(50,50)) #
+
+#ejemplo imagen:
+import pygame 
+pygame.init()
+
+#colores
+ANCHO_VENTADA=500
+ALTO_VENTANA=500
+COLOR_CELESTE=(0,0,128)
+
+#posiciones
+posicion_messi=[30,100] #COMO LA POSICION DE MESSI SI LA VOY A MODIFICAR ES UNA LISTA SI FUERA TUPLA COMO EN SCALE NO PODRIA LUEGO MODIFICAR SU TAMAÑO
+posicion_pasto=(0,0)
+#imagenes
+imagen_messi=pygame.image.load("messi.png") #el load me permite con el nombre de la imagen agarrar la superficie de la imagen y guardarla
+imagen_messi=pygame.transform.scale(imagen_messi,(200,200)) #esto me permite cambiar el escalado, el tamaño de la imagen Y COMO ES TUPLA NO PUEDO CAMBIARL LUEGO
+imagen_pasto=pygame.image.load("pasto.jpg")
+imagen_pasto=pygame.transform.scale(imagen_pasto,(500,500))
+#crear pantalla
+pantalla=pygame.display.set_mode((ANCHO_VENTADA,ALTO_VENTANA))
+#seteo un titulo de pantalla
+pygame.display.set_caption("Juego")
+flag_correr=True
+while flag_correr:
+	lista_eventos=pygame.event.get()
+	for evento in lista_eventos:
+		if evento.type==pygame.QUIT:
+			flag_correr=False
+		if evento.type==pygame.MOUSEBUTTONDOWN: #si presiono el boton del mouse y mantengo dependendiendo de donde cambio la posicion de messi
+			#print(evento.pos)
+			posicion_messi=evento.pos
+
+	#fondo color
+	pantalla.fill(COLOR_CELESTE)
+	pantalla.blit(imagen_pasto,(posicion_pasto)) #RECORDAR Q ESTA PARTE SON POR CAPAS, ES DECIR PRIMERO SIEMPRE VA EL FONDO 
+												#EN ESTE CASO SI FUERA AL REVES MESSI ESTARIA DEBAJO DEL FONDO
+	pantalla.blit(imagen_messi,(posicion_messi)) #fundir la imagen, pegamos la foto de messi a nuestra pantalla ya q agregamos algo externo q no pertenece a pygame
+												#por eso usamos el metodo blit
+
+	#mostrar
+	pygame.display.flip()
+pygame.quit()
 
 
+#TEXTOS:el metodo render me permite obtener una superficie a partir de un texto
+#ESTO NOS VA A PERMITIR EMPEZAR A JUGAR CON LOS TEXTOS Q VOY A PONER
+
+#font=pygame.font.SysFont("Arial Narrow",50)
+#text=font.render("HOLA MUNDO",True,(255,0,0))
+#print(type(text)) #class "pygame.Surface"
+
+#ejeplo texto
+
+import pygame 
+pygame.init()
+
+#colores
+ANCHO_VENTADA=500
+ALTO_VENTANA=500
+COLOR_CELESTE=(0,0,128)
+
+#posiciones
+posicion_messi=[30,100] #
+posicion_pasto=(0,0)
+
+#TEXTO
+font = pygame.font.SysFont("Arial Narrow",50) #la fuente q quiero ponerle y 50 es el tamaño
+
+#imagenes
+imagen_messi=pygame.image.load("messi.png") 
+imagen_messi=pygame.transform.scale(imagen_messi,(200,200)) 
+imagen_pasto=pygame.image.load("pasto.jpg")
+imagen_pasto=pygame.transform.scale(imagen_pasto,(500,500))
+#crear pantalla
+pantalla=pygame.display.set_mode((ANCHO_VENTADA,ALTO_VENTANA))
+#seteo un titulo de pantalla
+pygame.display.set_caption("Juego")
+flag_correr=True
+while flag_correr:
+	lista_eventos=pygame.event.get()
+	for evento in lista_eventos:
+		if evento.type==pygame.QUIT:
+			flag_correr=False
+		if evento.type==pygame.MOUSEBUTTONDOWN: 
+			#print(evento.pos)
+			posicion_messi=evento.pos
+
+	#fondo color
+
+	pantalla.fill(COLOR_CELESTE)
+	pantalla.blit(imagen_pasto,(posicion_pasto)) 
+	pantalla.blit(imagen_messi,(posicion_messi)) 
+
+	text=font.render("HOLA MUNDO",True,(255,0,0)) #DENTRO SE PONE EL TEXTO Q QUIERO el antialias lo vamos a tener en TRUE q hace q no se vean pixeleadas 
+												#por eso esta en TRUE, lo q va (es el color)
+
+	pantalla.blit(text(50,50)) #ACA FUNDIMOS EL TEXTO pantalla seria el nombre de la pantalla q es lo q le puse yo en este ejemplo 
+							#ACA PUESO USARLO COMO EL TIEMPO DE ALGO O EL PUNTAJE Y ASI 
+							#el blit es importante para fundirlo a la pantalla y para el texto
+	#mostrar
+	pygame.display.flip()
+
+pygame.quit()
 
 
-
-
-
-
-
-
-
-
-
-
+#ejemplo si de si mantengo la tecla presionado mueve el circulo
+import pygame
+#iniciar la pantalla
+pygame.init()
+#COLORES
+ANCHO_VENTANA = 500
+ALTO_VENTANA = 500
+COLOR_AMARILLO = (255, 255, 0)
+COLOR_CELESTE = ( 0, 0,128)
+#POSICIONES
+posicion_circulo = [100, 100]
+#crear la pantalla
+pantalla = pygame.display.set_mode((ANCHO_VENTANA, ALTO_VENTANA))
+#Seteo un título en la pantalla
+pygame.display.set_caption("Juego")
+flag_correr = True
+while flag_correr:
+    lista_eventos = pygame.event.get()
+    for evento in lista_eventos:
+        if evento.type == pygame.QUIT:
+            flag_correr = False
+    #salgo del for para ver que teclas están presionadas, se lee todo el tiempo
+    #funciona mientras las teclas están presionadas
+    lista_teclas = pygame.key.get_pressed() #ESTE METODO ME GUARDA TODAS LAS TECLAS Q PRESIONO POR ESO NO ESTA EN EVENTOS, ME GUARDO TODAS LAS TECLAS Q TOCO
+    #print(lista_teclas)
+    if True in lista_teclas: #PREGUNTO SI EN LA LISTA Q TENGO HAY ALGUNA Q ES VERDADERA 
+        if lista_teclas[pygame.K_RIGHT]:
+            posicion_circulo[0] = posicion_circulo[0] + 0.05 #se mueve 0.05 pixel #mientras #la tecla está presionada
+        if lista_teclas[pygame.K_LEFT]:
+            posicion_circulo[0] = posicion_circulo[0] - 0.05
+        if lista_teclas[pygame.K_UP]:
+            posicion_circulo[1] = posicion_circulo[1] - 0.05
+        if lista_teclas[pygame.K_DOWN]:
+            posicion_circulo[1] = posicion_circulo[1] + 0.05
+    #fondo color
+    pantalla.fill(COLOR_CELESTE)
+    pygame.draw.circle(pantalla, COLOR_AMARILLO,posicion_circulo,80)
+    #mostrar
+    pygame.display.flip()
+pygame.quit()
 
 
 
