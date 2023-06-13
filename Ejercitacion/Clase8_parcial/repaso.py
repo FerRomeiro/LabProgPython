e9c9b5c00439c84553bf1f76123f7ae4930913da
FerRomeiro <02FernandoRomero@gmail.com>
2023-06-13 15:14:32 -0300

    repaso

--- Ejercitacion/Clase8_parcial/repaso.py
+++ Ejercitacion/Clase8_parcial/repaso.py
@@ -26,6 +26,102 @@
 #
 #SQL, las BD relacionales utilizan un lenguaje llamado SQL mediante el cual se puede gestionar la base de datos
 #Formado por:
-#lenguaje de definicion de datos
-#lenguaje de manipulacion de datos
+#lenguaje de definicion de datos(DDL)
+#lenguaje de manipulacion de datos(DML)
 #
+#SQL lenguaje de consulta estructurado, esta formado basicamente por dos lenguajes que son DDL y DML
+#DDL nosotros vamos a poder hacer cosas con respecto a los objetos de la base de datos y con respecto a la tabla voy a poder crearla tmb voy a póder 
+#crear base de datos, alterar la tabla, voy a poder borrar la tabla.
+#DML voy a poder manipular los datos, voy a poder acceder a la base de datos, voy a poder agregar modificar eliminar datos
+#
+#DDL podemos crear,alterar,eliminar y truncar. La diferencia entre eliminar o drop y truncar es que si nosotros usamos la sentencia drop lo q hacemos es borrar
+#tabla y datos, y si usamos truncar o truncate lo q hacemos es eliminar la tabla pero los datos siguen existiendo
+#alter o crear es si yo quiero agregar campos, restricccion de campos 
+#
+#DML vamos a poder seleccionar o  select, insert o insertar, update o actualizar, delete o borrar. Con select voy a poder traer informacion de la base de datos
+# con insert puedo agregar datos a la base de datos, con update voy a poder modificar datos de la base de datos y con delete voy a poder borrar datos de la base de 
+#datos y cuando se dice base de datos nos referimos a la tabla que pertenece a la base de datos. Nosotros trabajamos con una sola tabla 
+#
+#TIPO DE DATOS DE SQLite
+#null:  es un valor nulo, significa q no hay dato
+#intenger:  entero con signo,  
+#real:  valor de coma flotante
+#text:  cadena de texto
+#blob:  son bytes almacenados exactamente como se ingresaron, aca podemos guardar imagenes y muchas cosas mas
+#
+#LA BASE DE DATOS ES UN CONTENEDOR DE TABLAS Y LA TABLA ES UN CONTENEDOR DE REGISTROS Y LOS REGISTROS ESTAN COMPUESTOS DE DATOS
+#LA DIFERENCIA ENTRE BASE DE DATOS Y PYTHON ES QUE PYTHON ES UN LENGUAJE NO TIPADO Y LA BASE DATOS TENEMOS Q DECIRLE EL TIPO DE DATOS Q SE VA A 
+#GUARDAR EN CADA CAMPO DONDE LA SUMATORIA DE ESOS CAMPOS VAN A DAR EL REGISTRO DE ESA FILA 
+#
+#Conectar SQLite: para poder trabajr con bases de datos de tipo SQLite debemos primero importar el modulo "sqlit3", luego conectar con la base de datos
+#despues operar, y por ultimo no olvidar cerrar la conexion
+#para conectar con la base de datos hay 2 maneras:
+#
+#import sqlite3
+#conexion = sqlit3.connect("bd_btf.db") #lo q esta entre () es el nombre de la base de datos
+#...
+#conexion.close()
+
+#la otra manera:
+#import sqlit3
+#with sqlit3.connect("bd_btf.db") as conexion: 
+#...
+#
+#crear tabla en SQlite
+#para poder crear una tabla en la base de datos:
+#import sqlit3
+#with sqlit3.connect("CLASE_sqlite/bd_btf") as conexion 
+#try:
+#   sentencia = ''' create table personaje #ACA CRE LA TABLA CON LAS 2 PALABRAS RESERVADAS create table
+#                   (
+#                           id integer primary key autoincrement #primary key significa q es unico y irrepetible, en este caso la ID va a ser unico y autonumerico
+#                           nombre text, 
+#                           apellido text,
+#                           anio real            
+#                   )                           
+#               ''' 
+#   conexion.execute(sentencia) #CON ESTO EJECUTO LA SENTENCIA
+#   print("Se creo la tabla de personajes")
+#   except sqlit3.OperationalError:
+#       print("La tabla personajes ya existe")
+
+#1:11:20
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
